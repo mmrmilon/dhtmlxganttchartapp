6bfad4891d2aec056e3ada302334d6df5d20e751
Mizanur Rahman <mmr_bd@hotmail.com>
2021-03-02 11:32:45 +0600

    Added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DhtmlxGanttChartApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CriticalPath()
+        {
+            ViewBag.Projects = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Value = "360",
+                    Text = "PCP21322083 - Project Critical Path Testing (01-Feb-2021 - 31-Dec-2021)",
+                    Selected = true
+                }
+            };
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
